7b4ac19d76173bae68889f9923acfd7f46bffe0f
9RACHA <mgl15r@gmail.com>
2023-05-10 17:35:14 +0200

    Player se mueve correctamente

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Unity.Netcode;
+using UnityEngine;
 
-public class PlayerMovement : NetworkBehaviour
+namespace PlayerMove
 {
-    [SerializeField] private float moveSpeed = 1f;
-    private Rigidbody rb;
-    private Vector3 moveDirection;
-    private NetworkObject networkObject;
-
-
-   // private bool IsHost => networkObject.IsServer;
-
-    private void Awake()
+    public class PlayerMovement : NetworkBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        networkObject = GetComponent<NetworkObject>();
-    }
+        // Variable de red que almacena la posición del jugador
+        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
-    void Start()
-    {
-        Debug.Log($"Soy el jugador {networkObject.NetworkObjectId} {(IsHost ? "(Host)" : "(Cliente)")}");
-    }
+        // Método que se ejecuta cuando el jugador es creado en la red
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner)
+            {
+                // El propietario del jugador solicita la posición inicial al servidor
+                RequestInitialPositionServerRpc();
+            }
+        }
 
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
+        // RPC que se ejecuta en el servidor para generar una posición inicial aleatoria
+        [ServerRpc]
+        void RequestInitialPositionServerRpc()
+        {
+            // Genera una posición aleatoria en un plano
+            Position.Value = GetRandomPositionOnPlane();
+
+            // Actualiza la posición en los clientes para que vean la misma posición inicial
+            UpdatePositionClientRpc(Position.Value);
+        }
+
+        // RPC que se ejecuta en el servidor para solicitar el cambio de posición
+        [ServerRpc]
+        void RequestPositionChangeServerRpc(Vector3 direction)
+        {
+            // Actualiza la posición sumando la dirección especificada
+            Position.Value += direction;
+
+            // Actualiza la posición en los clientes para sincronizar el movimiento
+            UpdatePositionClientRpc(Position.Value);
+        }
+
+        // RPC que se ejecuta en los clientes para sincronizar la posición
+        [ClientRpc]
+        void UpdatePositionClientRpc(Vector3 newPosition)
+        {
+            // Si el jugador no es el propietario, actualiza la posición recibida
+            if (!IsOwner)
+                Position.Value = newPosition;
+        }
+
+        // Genera una posición aleatoria en un plano
+        static Vector3 GetRandomPositionOnPlane()
+        {
+            return new Vector3(Random.Range(-5f, 5f), 1f, Random.Range(-5f, 5f));
+        }
+
+        void Update()
+        {
+            if (IsOwner)
+            {
+                Vector3 direction = Vector3.zero;
+
+                // Detecta las flechas para determinar la dirección del movimiento
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    direction = Vector3.left;
+                else if (Input.GetKeyDown(KeyCode.RightArrow))
+                    direction = Vector3.right;
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                    direction = Vector3.back;
+                else if (Input.GetKeyDown(KeyCode.UpArrow))
+                    direction = Vector3.forward;
+
+                // Si se ha presionado alguna tecla de flecha, solicita el cambio de posición al servidor
+                if (direction != Vector3.zero)
+                    RequestPositionChangeServerRpc(direction);
+            }
+
+            // Actualiza la posición del objeto en el mundo del juego basándose en el valor actual de la variable de red Position, 
+            // asegurando que el movimiento se sincronice correctamente en todos los clientes
+            transform.position = Position.Value;
+        }
     }
 }
-
